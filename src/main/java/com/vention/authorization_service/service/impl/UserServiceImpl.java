85f24d6fc2cf50b77ad3eba354c513c6841211b8
Abbos Akramov <abbosakramov121@gmail.com>
2023-11-30 14:59:40 +0500

    feature/JI-6-user-profile-filling and uploading photo, adding isDeleted field to users table and some other minor improvements

--- src/main/java/com/vention/authorization_service/service/impl/UserServiceImpl.java
+++ src/main/java/com/vention/authorization_service/service/impl/UserServiceImpl.java
@@ -14,8 +14,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Optional;
-
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService {
