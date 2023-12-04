4ec0f131f043a31922dba74c10af0ae5c0f77809
DeLivva <151529065+DeLivva@users.noreply.github.com>
2023-12-04 12:30:16 +0500

    configured for testing environment on the cloud (#14)
    Co-authored-by: Shakhzod Elturaev <shahzodelturayev@gmail.com>
    Co-authored-by: Abbos Akramov <87889743+Abbos121@users.noreply.github.com>

---
+++ src/main/java/com/vention/authorization_service/exception/FileConvertingException.java
@@ -0,0 +1,7 @@
+package com.vention.authorization_service.exception;
+
+public class FileConvertingException extends RuntimeException {
+    public FileConvertingException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/vention/authorization_service/service/impl/FileServiceImpl.java
+++ src/main/java/com/vention/authorization_service/service/impl/FileServiceImpl.java
@@ -1,33 +1,49 @@
 package com.vention.authorization_service.service.impl;
 
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.model.PutObjectRequest;
+import com.amazonaws.services.s3.model.PutObjectResult;
+import com.vention.authorization_service.exception.FileConvertingException;
 import com.vention.authorization_service.service.FileService;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
 import java.io.IOException;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Objects;
 
-@Service
 @Slf4j
+@Service
+@RequiredArgsConstructor
 public class FileServiceImpl implements FileService {
 
-    @Value("${files.image-path}")
-    private String fileDirectory;
+    @Value("${cloud.aws.s3bucket.name}")
+    private String bucketName;
+
+    @Value("${cloud.aws.s3bucket.url}")
+    private String bucketUrl;
+
+    private final AmazonS3 amazonS3;
 
     @Override
     public String uploadFile(MultipartFile file) {
-        try {
-            byte[] bytes = file.getBytes();
-            Path path = Paths.get(fileDirectory + file.getOriginalFilename());
-            Files.write(path, bytes);
-            return path.toString();
-        } catch (IOException e) {
-            log.warn(e.getMessage());
-            return "Failed to upload '" + file.getOriginalFilename() + "'";
+        String fileName = file.getOriginalFilename();
+        File convertedFile = convertMultipartFileToFile(file);
+        amazonS3.putObject(new PutObjectRequest(bucketName, fileName, convertedFile));
+        return bucketUrl + fileName;
+    }
+
+    private File convertMultipartFileToFile(MultipartFile multipartFile) {
+        File convertedFile = new File(Objects.requireNonNull(multipartFile.getOriginalFilename()));
+        try (FileOutputStream outputStream = new FileOutputStream(convertedFile)) {
+            outputStream.write(multipartFile.getBytes());
+            return convertedFile;
+        } catch(IOException e) {
+            throw new FileConvertingException(e.getMessage());
         }
     }
 }
