ffb9fb08171271f09a6bf66e55ce0116da0d2124
HasilbekR <asetrich01@gmail.com>
2023-12-07 16:14:44 +0500

    login via email/username with password and oauth created, security via jwt token implemented

--- src/main/java/com/vention/authorization_service/config/SecurityConfig.java
+++ src/main/java/com/vention/authorization_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.vention.authorization_service.filter.JwtFilterToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -15,16 +16,21 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 public class SecurityConfig {
 
     private final JwtFilterToken filterToken;
+    private final String[] permitAll = {
+            "/api/v1/auth/login",
+            "/api/v1/auth/register",
+            "/api/v1/auth/confirm-email",
+            "/api/v1/auth/resend-confirmation",
+            };
 
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
                 .csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests((authorize) -> {
-                    authorize
-                            .requestMatchers("/api/v1/auth/login", "/api/v1/auth/register", "/api/v1/auth/confirm-email", "/api/v1/users").permitAll()
-                            .anyRequest().authenticated();
-                })
+                .authorizeHttpRequests((authorize) -> authorize
+                        .requestMatchers(permitAll).permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v1/users").permitAll()
+                        .anyRequest().authenticated())
                 .oauth2Login(Customizer.withDefaults())
                 .addFilterBefore(filterToken, UsernamePasswordAuthenticationFilter.class)
                 .build();
