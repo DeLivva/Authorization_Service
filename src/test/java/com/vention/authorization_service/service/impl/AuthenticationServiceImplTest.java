504ecece2598c1d3a8df141efbe6d78ff0b5d4bd
HasilbekR <asetrich01@gmail.com>
2023-12-08 18:07:39 +0500

    tests added

--- src/test/java/com/vention/authorization_service/service/impl/AuthenticationServiceImplTest.java
+++ src/test/java/com/vention/authorization_service/service/impl/AuthenticationServiceImplTest.java
@@ -3,30 +3,46 @@ package com.vention.authorization_service.service.impl;
 import com.vention.authorization_service.domain.SecurityCredentialEntity;
 import com.vention.authorization_service.domain.UserEntity;
 import com.vention.authorization_service.domain.UserRoleEntity;
+import com.vention.authorization_service.domain.UserState;
+import com.vention.authorization_service.dto.request.UserLoginRequestDto;
 import com.vention.authorization_service.dto.request.UserRegistrationRequestDTO;
+import com.vention.authorization_service.dto.response.JwtResponse;
 import com.vention.authorization_service.dto.response.UserRegistrationResponseDTO;
+import com.vention.authorization_service.exception.DataNotFoundException;
 import com.vention.authorization_service.exception.DuplicateDataException;
+import com.vention.authorization_service.exception.LoginFailedException;
 import com.vention.authorization_service.mapper.SecurityCredentialMapper;
 import com.vention.authorization_service.mapper.UserMapper;
+import com.vention.authorization_service.service.JwtService;
 import com.vention.authorization_service.service.MailSendingService;
 import com.vention.authorization_service.service.SecurityCredentialService;
 import com.vention.authorization_service.service.UserRoleService;
 import com.vention.authorization_service.service.UserService;
+import io.jsonwebtoken.Claims;
+import jakarta.servlet.http.HttpServletRequest;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
+
+import java.util.Map;
 
 import static org.assertj.core.api.Fail.fail;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.never;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class AuthenticationServiceImplTest {
@@ -39,7 +55,6 @@ class AuthenticationServiceImplTest {
 
     @Mock
     private UserRoleService userRoleService;
-
     @Mock
     private UserMapper userMapper;
 
@@ -51,6 +66,12 @@ class AuthenticationServiceImplTest {
 
     @InjectMocks
     private AuthenticationServiceImpl authenticationService;
+
+    @Mock
+    private JwtService jwtService;
+
+    @Mock
+    private CustomUserDetailsServiceImpl userDetailsService;
 
     @Test
     void testRegisterUser() {
@@ -89,5 +110,183 @@ class AuthenticationServiceImplTest {
         verify(userRoleService, never()).getByName(any());
         verify(securityCredentialService, never()).saveCredentials(any());
         verify(userService, never()).saveUser(any());
+    }
+
+    @Test
+    void testLoginEmailSuccess() {
+        // given
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test@gmail.com", "test");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserState(UserState.AUTHORIZED);
+
+        when(userService.getByEmail(userLoginRequestDto.getLogin())).thenReturn(userEntity);
+        when(jwtService.generateAccessToken(any(UserEntity.class))).thenReturn("testToken");
+
+        // when
+        JwtResponse response = authenticationService.login(userLoginRequestDto);
+
+        // then
+        assertNotNull(response);
+        assertEquals("testToken", response.getAccessToken());
+    }
+
+    @Test
+    void testLoginEmailNotFoundFail() {
+        // given
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test@gmail.com", "test");
+
+        when(userService.getByEmail(userLoginRequestDto.getLogin())).thenThrow(DataNotFoundException.class);
+
+        // then
+        assertThrows(DataNotFoundException.class, () -> authenticationService.login(userLoginRequestDto));
+    }
+
+    @Test
+    void testLoginUsernameSuccess() {
+        // given
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test", "test");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserState(UserState.AUTHORIZED);
+
+        when(userService.getByUsername(userLoginRequestDto.getLogin())).thenReturn(userEntity);
+        when(jwtService.generateAccessToken(any(UserEntity.class))).thenReturn("testToken");
+
+        // when
+        JwtResponse response = authenticationService.login(userLoginRequestDto);
+
+        // then
+        assertNotNull(response);
+        assertEquals("testToken", response.getAccessToken());
+    }
+
+    @Test
+    void testLoginUsernameNotFoundFail() {
+        // when
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test", "test");
+
+        when(userService.getByUsername(userLoginRequestDto.getLogin())).thenThrow(DataNotFoundException.class);
+
+        // then
+        assertThrows(DataNotFoundException.class, () -> authenticationService.login(userLoginRequestDto));
+    }
+
+    @Test
+    void testLoginAccountNotVerifiedFail() {
+        // when
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test@gmail.com", "test");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserState(UserState.CREATED);
+
+        when(userService.getByEmail(userLoginRequestDto.getLogin())).thenThrow(LoginFailedException.class);
+
+        // then
+        assertThrows(LoginFailedException.class, () -> authenticationService.login(userLoginRequestDto));
+    }
+
+    @Test
+    void testLoginAccountNotCompletedRegisterFail() {
+        // when
+        UserLoginRequestDto userLoginRequestDto = new UserLoginRequestDto("test", "test");
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserState(UserState.VERIFIED);
+
+        when(userService.getByUsername(userLoginRequestDto.getLogin())).thenReturn(userEntity);
+
+        // then
+        try {
+            authenticationService.login(userLoginRequestDto);
+        } catch (LoginFailedException e) {
+            assertEquals("VERIFIED", e.getMessage());
+        }
+    }
+
+    @Test
+    void testLoginOAuthSuccess() {
+        // given
+        String userEmail = "test@gmail.com";
+        OAuth2AuthenticationToken oauthToken = mock();
+        UserEntity userEntity = new UserEntity();
+        userEntity.setUserState(UserState.AUTHORIZED);
+
+        when(oauthToken.getPrincipal()).thenReturn(mock());
+        when(oauthToken.getPrincipal().getAttributes()).thenReturn(Map.of("email", userEmail));
+
+        when(userService.getByEmail(userEmail)).thenReturn(userEntity);
+        when(jwtService.generateAccessToken(any(UserEntity.class))).thenReturn("testToken");
+
+        // when
+        JwtResponse result = authenticationService.loginOAuth(oauthToken);
+
+        // Then
+        assertNotNull(result);
+        assertEquals("testToken", result.getAccessToken());
+    }
+
+    @Test
+    void testLoginOAuthNotFoundFail() {
+        // when
+        String userEmail = "test@gmail.com";
+        OAuth2AuthenticationToken oauthToken = mock();
+
+        when(oauthToken.getPrincipal()).thenReturn(mock());
+        when(oauthToken.getPrincipal().getAttributes()).thenReturn(Map.of("email", userEmail));
+
+        when(userService.getByEmail(userEmail)).thenThrow(DataNotFoundException.class);
+        // then
+        assertThrows(DataNotFoundException.class, () -> authenticationService.loginOAuth(oauthToken));
+    }
+
+    @Test
+    void testLoginOAuthMissFail() {
+        // when
+        // then
+        assertThrows(NullPointerException.class, () -> authenticationService.loginOAuth(null));
+    }
+
+    @Test
+    void testAuthenticateValidTokenSuccess() {
+        // Given
+        String token = "validToken";
+        Claims claims = mock();
+        when(jwtService.extractToken(token)).thenReturn(mock());
+        when(jwtService.extractToken(token).getBody()).thenReturn(claims);
+        when(claims.getSubject()).thenReturn("test@gmail.com");
+
+        UserDetails userDetails = mock();
+        when(userDetailsService.loadUserByUsername("test@gmail.com")).thenReturn(userDetails);
+        when(jwtService.isTokenValid(token)).thenReturn(true);
+
+        HttpServletRequest request = mock();
+
+        // When
+        authenticationService.authenticate(token, request);
+
+        // Then
+        assertNotNull(SecurityContextHolder.getContext().getAuthentication());
+        verify(userDetails, times(1)).getUsername();
+        verify(jwtService, times(1)).isTokenValid(token);
+    }
+
+    @Test
+    void testAuthenticateExpiredTokenFail() {
+        SecurityContextHolder.clearContext();
+        // Given
+        String token = "expiredToken";
+        Claims claims = mock();
+        when(jwtService.extractToken(token)).thenReturn(mock());
+        when(jwtService.extractToken(token).getBody()).thenReturn(claims);
+        when(claims.getSubject()).thenReturn("test@gmail.com");
+
+        UserDetails userDetails = mock();
+        when(userDetailsService.loadUserByUsername("test@gmail.com")).thenReturn(userDetails);
+        when(jwtService.isTokenValid(token)).thenReturn(false);
+
+        HttpServletRequest request = mock();
+
+        // When
+        authenticationService.authenticate(token, request);
+
+        // Then
+        assertNull(SecurityContextHolder.getContext().getAuthentication());
     }
 }
