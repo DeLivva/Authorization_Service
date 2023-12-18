c4f15aa6fd19532280330ee556c302fe65b0c393
HasilbekR <asetrich01@gmail.com>
2023-12-18 12:03:05 +0500

    new permission added

--- src/main/java/com/vention/authorization_service/config/SecurityConfig.java
+++ src/main/java/com/vention/authorization_service/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.vention.authorization_service.filter.JwtFilterToken;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -29,6 +30,7 @@ public class SecurityConfig {
             "/api/v1/auth/confirm-email",
             "/api/v1/auth/resend-confirmation",
             "/api/v1/users/fill-profile",
+            "/api/v1/security-credentials"
             };
 
     @Bean
@@ -38,6 +40,7 @@ public class SecurityConfig {
                 .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers(permitAll).permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v1/users/**").permitAll()
                         .anyRequest().authenticated())
                 .addFilterBefore(filterToken, UsernamePasswordAuthenticationFilter.class)
                 .build();
